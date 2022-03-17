8f2fdc5fccb9d4dd6b47c942587c3604faac4088
Narek Sargsyan <m.nareksargsyan@gmail.com>
2022-03-17 16:07:23 +0100

    #3.5 Fonction Return Valeur

--- cmysql.h
+++ cmysql.h
@@ -5,29 +5,44 @@
 
 class CMysql {
     private:
-        QSqlDatabase DB;
-        QSqlQuery *Query;
+        QSqlDatabase m_DB;
+        QSqlQuery *m_Query;
 
     public:
         CMysql() {}
         ~CMysql() {}
 
         void Connect(QString Host, QString Database, QString User, QString Password) {
-            this->DB = QSqlDatabase::addDatabase("QMYSQL");
-            this->DB.setHostName(Host);
-            this->DB.setDatabaseName(Database);
-            this->DB.setUserName(User);
-            this->DB.setPassword(Password);
-            this->DB.open();
-
-            this->Query = new QSqlQuery(this->DB);
+            this->m_DB = QSqlDatabase::addDatabase("QMYSQL");
+            this->m_DB.setHostName(Host);
+            this->m_DB.setDatabaseName(Database);
+            this->m_DB.setUserName(User);
+            this->m_DB.setPassword(Password);
+            if(this->m_DB.open()) {
+                this->m_Query = new QSqlQuery(this->m_DB);
+            }
         }
 
         void Disconnect() {
-            delete this->Query;
-            this->DB.close();
+            delete this->m_Query;
+            this->m_DB.close();
         }
 
+        bool Exec(QString Req) {
+            return (this->m_Query->exec(Req));
+        }
+
+        bool Next() {
+            return (this->m_Query->next());
+        }
+
+        QString ValueString(int Index) {
+            return (this->m_Query->value(Index).toString());
+        }
+
+        int ValueInt(int Index) {
+            return (this->m_Query->value(Index).toInt());
+        }
 };
 
 #endif // CMYSQL_H
--- cprojet.cpp
+++ cprojet.cpp
@@ -3,6 +3,22 @@
 
 CProjet::CProjet(QWidget *parent) : QMainWindow(parent), ui(new Ui::CProjet) {
     ui->setupUi(this);
+
+    CMysql Database;
+
+    Database.Connect("localhost", "elevage", "root", "");
+
+    if(Database.Exec("SELECT * FROM client")) {
+        while (Database.Next()) {
+
+        }
+    }
+
+    if(Database.Exec("SELECT * FROM prestation")) {
+        while (Database.Next()) {
+
+        }
+    }
 }
 
 CProjet::~CProjet() {
