e50aa39d2799de33d87daa272b1abe3435177df8
Narek Sargsyan <m.nareksargsyan@gmail.com>
2022-03-17 14:41:23 +0100

    #3 Attributs + Méthodes CMysql

--- cmysql.h
+++ cmysql.h
@@ -1,7 +1,32 @@
 #ifndef CMYSQL_H
 #define CMYSQL_H
 
+#include <QtSql>
+
 class CMysql {
+    private:
+        QSqlDatabase DB;
+        QSqlQuery *Query;
+
+    public:
+        CMysql() {}
+        ~CMysql() {}
+
+        void Connect(QString Host, QString Database, QString User, QString Password) {
+            this->DB = QSqlDatabase::addDatabase("QMYSQL");
+            this->DB.setHostName(Host);
+            this->DB.setDatabaseName(Database);
+            this->DB.setUserName(User);
+            this->DB.setPassword(Password);
+            this->DB.open();
+
+            this->Query = new QSqlQuery(this->DB);
+        }
+
+        void Disconnect() {
+            delete this->Query;
+            this->DB.close();
+        }
 
 };
 
