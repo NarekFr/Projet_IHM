693d36124c1bd0c3b2eb6b03c6cc832ca1b2aadd
Narek Sargsyan <m.nareksargsyan@gmail.com>
2022-03-17 16:20:34 +0100

    #4 Attributs + Méthodes CClient

--- cclient.h
+++ cclient.h
@@ -1,8 +1,36 @@
 #ifndef CCLIENT_H
 #define CCLIENT_H
 
-class CClient {
+#include <QMainWindow>
+#include <QVector>
 
+#include "cprestation.h"
+#include "cpensionnaire.h"
+
+class CClient {
+    private:
+        QString m_Name;
+        QString m_Mail;
+        QVector<CPrestation> m_Prestation;
+        QVector<CPensionnaire> m_Pensionnaire;
+
+    public:
+        CClient() {}
+        ~CClient() {}
+
+        CClient(QString Name, QString Mail) {
+            this->m_Name = Name;
+            this->m_Mail = Mail;
+        }
+
+        void AjouterPrestation() {}
+        void AjouterPensionnaire() {}
+
+        void SupprimerPresation() {}
+        void SupprimerPensionnaire() {}
+
+        void ModifierPrestation() {}
+        void ModifierPensionnaire() {}
 };
 
 #endif // CCLIENT_H
